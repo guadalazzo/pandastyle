6e3f65f452d71bb4ff3512779aec7752684ed58e
Guadalupe Jazmin Lazzo <guadalazzo@est.frba.utn.edu.ar>
2017-01-14 17:43:40 -0300

    a lot of changes at hairstyle & clothing

---
+++ THEPANDASTYLE/program.js
@@ -0,0 +1,12 @@
+var c="gato"
+var a="sos"
+var b="re"
+var d=" "
+for (var i =0; i < 3;i++){
+	console.log((a+d+b+d+c)+i);
+}
+var array= ["baila morena baila morena","meneo pa' los nenes meneo pa' las nenas","menea menea","mueva mueva","mueva el toto",7];
+for (var i = 0; i < 6; i++) {
+	console.log(array[i]);
+}
+
